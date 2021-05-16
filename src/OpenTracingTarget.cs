d7cc41d93aaf2c85543be44141a57ce8a84ba111
Rolf Kristensen <sweaty1@hotmail.com>
2021-05-16 11:01:15 +0200

    Initial commit

---
+++ src/OpenTracingTarget.cs
@@ -0,0 +1,45 @@
+﻿namespace NLog.Targets
+{
+    [Target("OpenTracing")]
+    public class OpenTracingTarget : TargetWithContext
+    {
+        private readonly OpenTracing.ITracer _tracer;
+
+        public bool SetTagErrorOnException { get; set; }
+
+        public OpenTracingTarget()
+            :this(null)
+        {
+        }
+
+        public OpenTracingTarget(OpenTracing.ITracer tracer)
+        {
+            _tracer = tracer ?? OpenTracing.Util.GlobalTracer.Instance;
+            ContextProperties.Add(new TargetPropertyWithContext("component", "${logger}"));
+            ContextProperties.Add(new TargetPropertyWithContext("level", "${level}"));
+            ContextProperties.Add(new TargetPropertyWithContext("message", "${message}"));
+            ContextProperties.Add(new TargetPropertyWithContext("eventid", "${event-properties:EventId_Id}") { IncludeEmptyValue = false });
+        }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            var span = _tracer.ActiveSpan;
+            if (span == null)
+                return;
+
+            if (SetTagErrorOnException && logEvent.Exception != null)
+            {
+                span.SetTag(OpenTracing.Tag.Tags.Error, true);
+            }
+
+            var fields = GetAllProperties(logEvent);
+            if (logEvent.Exception != null)
+            {
+                fields[OpenTracing.LogFields.ErrorKind] = logEvent.Exception.GetType().ToString();
+                fields[OpenTracing.LogFields.ErrorObject] = logEvent.Exception;
+            }
+
+            span.Log(fields);
+        }
+    }
+}
---
+++ test/NLog-OpenTracing-Tests/NLogOpenTracingTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using NLog.Targets;
+using Xunit;
+
+namespace NLog_OpenTracing_Tests
+{
+    public class NLogOpenTracingTests
+    {
+        [Fact]
+        public void NLogOpenTracingRuntime()
+        {
+            var tracer = new OpenTracing.Mock.MockTracer();
+            var logFactory = new NLog.LogFactory();
+            var logConfig = new NLog.Config.LoggingConfiguration(logFactory);
+            var logTarget = new NLog.Targets.OpenTracingTarget(tracer);
+            logConfig.AddRuleForAllLevels(logTarget);
+            logFactory.Configuration = logConfig;
+
+            using (var parentScope = tracer.BuildSpan("Parent").StartActive(finishSpanOnDispose: true))
+            {
+                var logger = logFactory.GetCurrentClassLogger();
+                logger.Info("Hello World");
+            }
+
+            var spans = tracer.FinishedSpans();
+            Assert.Single(spans);
+            var logEntries = spans.First().LogEntries;
+            Assert.Single(logEntries);
+            Assert.Equal("Hello World", logEntries.First().Fields["message"]);
+        }
+
+        [Fact]
+        public void NLogOpenTracingFromConfig()
+        {
+            var logFactory = new NLog.LogFactory();
+            var nlogConfig = @"<nlog>
+<targets>
+<target name='openTracing' type='openTracing'>
+   <contextproperty name='threadid' layout='${threadid}' />
+</target>
+</targets>
+<rules>
+<logger name='*' writeTo='openTracing' />
+</rules>
+</nlog>";
+            System.Xml.XmlReader xmlReader = System.Xml.XmlReader.Create(new System.IO.StringReader(nlogConfig));
+            var logConfig = new NLog.Config.XmlLoggingConfiguration(xmlReader, null, logFactory);
+            logFactory.Configuration = logConfig;
+
+            var tracer = new OpenTracing.Mock.MockTracer();
+            OpenTracing.Util.GlobalTracer.RegisterIfAbsent(tracer);
+
+            using (var parentScope = tracer.BuildSpan("Parent").StartActive(finishSpanOnDispose: true))
+            {
+                var logger = logFactory.GetCurrentClassLogger();
+                logger.Info("Hello World");
+            }
+
+            var spans = tracer.FinishedSpans();
+            Assert.Single(spans);
+            var logEntries = spans.First().LogEntries;
+            Assert.Single(logEntries);
+            Assert.Equal("Hello World", logEntries.First().Fields["message"]);
+        }
+    }
+}
